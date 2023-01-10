ee1f456cf9ced23cd3d8dc812550ed6ce9297099
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2023-01-10 15:59:51 +0000

    Update to auth journey

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,14 +2,20 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.post('/auth/details', (req, res) => { res.redirect('/auth/email') })
   router.post('/auth/email', (req, res) => { res.redirect('/auth/email-code') })
   router.post('/auth/email-code', (req, res) => { res.redirect('/auth/name') })
-  router.post('/auth/name', (req, res) => { res.redirect('/auth/date-of-birth') })
-  router.post('/auth/date-of-birth', (req, res) => { res.redirect('/auth/check-answers') })
-  router.post('/auth/check-answers', (req, res) => { res.redirect('/auth/details-finish') })
-  router.post('/auth/details-finish', (req, res) => { res.redirect('/auth/apply-for-qts') })
 
+  router.post(['/auth/name'], (req, res, next) => {
+    req.session.data['fullName'] = `${req.body['firstName']} ${req.body['lastName']}`
+    res.redirect('/auth/date-of-birth')
+  })
+
+  router.post('/auth/date-of-birth', (req, res) => { res.redirect('/auth/check-answers') })
+  router.post('/auth/check-answers', (req, res) => { res.redirect('/auth/finish') })
+  router.post('/auth/finish', (req, res) => { res.redirect('/auth/apply-for-qts') })
+
+
+  
   
 
 }
