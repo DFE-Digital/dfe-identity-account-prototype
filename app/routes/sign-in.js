02c8a0833c289e4513c2e94e2711ccefdd724c05
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2023-01-17 17:04:52 +0000

    update to number

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -2,8 +2,18 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.post('/sign-in/email', (req, res) => { res.redirect('/sign-in/email-code') })
+  router.post('/sign-in/email', (req, res) => { res.redirect('/sign-in/code-type') })
+
+  router.post('/sign-in/code-type', (req, res) => {
+    if(req.session.data['codeType'] == 'email') {
+      res.redirect('/sign-in/email-code')
+    } else {
+      res.redirect('/sign-in/phone-code')
+    }
+  })
+
   router.post('/sign-in/email-code', (req, res) => { res.redirect('/sign-in/finish') })
+  router.post('/sign-in/phone-code', (req, res) => { res.redirect('/sign-in/finish') })
 
 
   router.post('/sign-in/finish', (req, res) => { res.redirect('../auth/apply-for-qts') })
