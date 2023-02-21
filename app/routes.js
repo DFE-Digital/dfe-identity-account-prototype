b0f16b05ee26ebdcbb53be286908c5f724a1a0aa
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2023-02-21 16:28:02 +0000

    update to routes

--- app/routes.js
+++ app/routes.js
@@ -14,3 +14,8 @@ require('./routes/qts')(router)
 require('./routes/user-research')(router)
 
 
+
+router.post('/recieved-data', (req, res) => {
+    req.session.data['account-data'] = JSON.parse(req.body['account-data'])
+    res.redirect('/v3/dashboard/index')
+  }) 
