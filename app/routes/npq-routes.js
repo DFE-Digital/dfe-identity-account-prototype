26de64c2cf6ad9ddcb91a1bcb8f121a976ecf194
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2023-03-24 16:47:30 +0000

    Update npq-routes.js

--- app/routes/npq-routes.js
+++ app/routes/npq-routes.js
@@ -3,9 +3,8 @@ const _ = require('lodash')
 module.exports = router => {
 
 
- // Qual my account external link
 
-   // Qual create account
+   // NPQ create account
    router.get('/user-research/npq/id', (req, res) => {
     const data = req.session.data
     data.identityServiceName = data.npq
@@ -15,27 +14,40 @@ module.exports = router => {
     data.dqtCheck = 'true'
     data.emailAuth = 'false'
     data.signIn = 'true'
-    data.createAccountURL = '/user-research/qual/create-account'
-    data.signInAccountURL = '/user-research/qual/sign-in'
+    data.createAccountURL = '/user-research/npq/create-account'
+    data.signInAccountURL = '/user-research/npq/sign-in'
     data.verifiedDob = 'true'
-    res.redirect('/auth/email')
+    res.redirect('/auth/ga-account')
   })
 
 
 
-  // Qual sign in
+  // NPQ sign in
   router.get('/user-research/npq/sign-in', (req, res) => {
     const data = req.session.data
     data.identityServiceName = data.npq
     data.onwardContinue = data.npq
     data.returnToService = 'https://npq-prototype.herokuapp.com/registration-status/registration-status'
-    data.scenario = '1'
+    data.scenario = '2'
     data.emailAuth = 'false'
     data.verifiedDob = 'true'
     data.signIn = 'true'
     res.redirect('/sign-in/email')
   })
 
+   // NPQ create account
+   router.get('/user-research/npq/create-account', (req, res) => {
+    const data = req.session.data
+    data.identityServiceName = data.qs
+    data.onwardContinue = data.qs
+    data.returnToService = 'https://npq-prototype.herokuapp.com/chosen'
+    data.scenario = '3'
+    data.dqtCheck = 'true'
+    data.emailAuth = 'false'
+    data.signIn = 'true'
+    data.verifiedDob = 'true'
+    res.redirect('/auth/email')
+  })
 
   router.get('/auth/return-to-service', (req, res) => {
     res.redirect(req.session.data.returnToService)
