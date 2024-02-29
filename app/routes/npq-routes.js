f908ab3d82b004338bc1462ac4ae521620065bc0
Jackie DAlcorn <jacqueline@dalcorn.com>
2024-02-29 21:18:29 +0000

    NPQ closed links

--- app/routes/npq-routes.js
+++ app/routes/npq-routes.js
@@ -73,7 +73,7 @@ module.exports = router => {
     res.redirect('/account/account-details')
   })
 
-  // NPQ closed state - sign in
+  // NPQ closed state - start page sign in link
   router.get('/user-research/npq/closed-signin', (req, res) => {
     const data = req.session.data
     data.identityServiceName = data.npq
@@ -81,26 +81,79 @@ module.exports = router => {
     data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-signin'
     data.scenario = '4'
     data.emailAuth = 'false'
-    data.createAccountURL = '/user-research/npq/create-account'
-    data.signInAccountURL = '/user-research/npq/sign-in'
+    data.createAccountURL = '/user-research/npq/create-account-closed'
+    data.signInAccountURL = '/user-research/npq/sign-in-closed'
     data.verifiedDob = 'true'
     data.signIn = 'true'
     res.redirect('/auth/ga-account')
   })
 
-  // NPQ closed state - EoI request
+  // NPQ closed state - create account
+  router.get('/user-research/npq/create-account-closed', (req, res) => {
+    const data = req.session.data
+    data.identityServiceName = data.npq
+    data.onwardContinue = data.npq
+    data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-signin'
+    data.scenario = '5'
+    data.dqtCheck = 'true'
+    data.emailAuth = 'false'
+    data.signIn = 'true'
+    data.verifiedDob = 'true'
+    res.redirect('/auth/email')
+  })
+
+  // NPQ closed state - sign in 
+  router.get('/user-research/npq/sign-in-closed', (req, res) => {
+    const data = req.session.data
+    data.identityServiceName = data.npq
+    data.onwardContinue = data.npq
+    data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-signin'
+    data.scenario = '6'
+    data.emailAuth = 'false'
+    data.verifiedDob = 'true'
+    data.signIn = 'true'
+    res.redirect('/sign-in/email')
+  })
+
+  // NPQ closed state - start page EoI request link 
   router.get('/user-research/npq/closed-eoi', (req, res) => {
     const data = req.session.data
     data.identityServiceName = data.npq
     data.onwardContinue = data.npq
-    data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-eoi'
-    data.scenario = '5'
+    data.scenario = '7'
     data.emailAuth = 'false'
-    data.createAccountURL = '/user-research/npq/create-account'
-    data.signInAccountURL = '/user-research/npq/sign-in'
+    data.createAccountURL = '/user-research/npq/create-account-closed-eoi'
+    data.signInAccountURL = '/user-research/npq/sign-in-closed-eoi'
     data.verifiedDob = 'true'
     data.signIn = 'true'
     res.redirect('/auth/ga-account')
+  })
+
+  // NPQ closed state EOI - create account
+  router.get('/user-research/npq/create-account-closed-eoi', (req, res) => {
+    const data = req.session.data
+    data.identityServiceName = data.npq
+    data.onwardContinue = data.npq
+    data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-eoi'
+    data.scenario = '8'
+    data.dqtCheck = 'true'
+    data.emailAuth = 'false'
+    data.signIn = 'true'
+    data.verifiedDob = 'true'
+    res.redirect('/auth/email')
+  })
+
+  // NPQ closed state EOI - sign in
+  router.get('/user-research/npq/sign-in-closed-eoi', (req, res) => {
+    const data = req.session.data
+    data.identityServiceName = data.npq
+    data.onwardContinue = data.npq
+    data.returnToService = 'https://npq-prototype.herokuapp.com/user-research/npq/closed-eoi'
+    data.scenario = '9'
+    data.emailAuth = 'false'
+    data.verifiedDob = 'true'
+    data.signIn = 'true'
+    res.redirect('/sign-in/email')
   })
 
   router.get('/auth/return-to-service', (req, res) => {
